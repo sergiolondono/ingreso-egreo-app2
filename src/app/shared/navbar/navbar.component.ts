de13e5487bc9c7d5ab8499f818d84e26ae8674c2
Sergio Londono <sergio.londonog@gmail.com>
2022-05-08 16:21:47 -0500

    Ajuste tamaño grafica

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { filter, Subscription } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  nombre?: string = '';
+  userSubs!: Subscription;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.userSubs = this.store.select('user')
+    .pipe(
+      filter(({ user }) => user != null)
+    )
+    .subscribe(({ user }) => this.nombre = user?.nombre );
+  }
+
+  ngOnDestroy(): void {
+    this.userSubs.unsubscribe();
   }
 
 }
